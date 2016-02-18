462f123b83560e86e99d7a8cb743905bab7e46ad
Travis <mail@travisbloomfield.com>
2016-02-19 06:59:23 +1100

    Added action types that use energy.

--- electronic_life.js
+++ electronic_life.js
@@ -222,9 +222,6 @@ function LifelikeWorld(map, legend) {
   World.call(this, map, legend);
 }
 LifelikeWorld.prototype = Object.create(World.prototype);
-
-var actionTypes = Object.create(null);
-
 LifelikeWorld.prototype.letAct = function(critter, vector) {
   var action = critter.act(new View(this, vector));
   var handled = action &&
@@ -236,4 +233,44 @@ LifelikeWorld.prototype.letAct = function(critter, vector) {
     if (critter.energy <= 0)
       this.grid.set(vector, null);
   }
+};
+
+// Define action types for critters
+var actionTypes = Object.create(null);
+// Grow is used for plants.
+actionTypes.grow = function(critter) {
+  critter.energy += 0.5;
+  return true;
+};
+actionTypes.move = function(critter, vector, action) {
+  var dest = this.checkDestination(action, vector);
+  if (dest === null ||
+      critter.energy <= 1 ||
+      this.grid.get(dest) !== null)
+    return false;
+  critter.energy -= 1;
+  this.grid.set(vector, null);
+  this.grid.set(dest, critter);
+  return true;
+};
+actionTypes.eat = function(critter, vector, action) {
+  var dest = this.checkDestination(action, vector);
+  var atDest = dest !== null && this.grid.get(dest);
+  if (!atDest || atDest.energy === null)
+    return false;
+  critter.energy += atDest.energy;
+  this.grid.set(dest, null);
+  return true;
+};
+actionTypes.reproduce = function(critter, vector, action) {
+  var baby = elementFromChar(this.legend,
+                             critter.originChar);
+  var dest = this.checkDestination(action, vector);
+  if (dest === null ||
+      critter.energy <= 2 * baby.energy ||
+      this.grid.get(dest) !== null)
+    return false;
+  critter.energy -= 2 * baby.energy;
+  this.grid.set(dest, baby);
+  return true;
 };
