de07710cab37be15e4c5f8ecb8e3ba47b41fab3d
Travis <mail@travisbloomfield.com>
2016-02-18 20:01:30 +1100

    Create new world object to implement energy use.

--- electronic_life.js
+++ electronic_life.js
@@ -216,3 +216,24 @@ WallFollower.prototype.act = function(view) {
   }
   return {type: "move", direction: this.dir};
 };
+
+// LifelikeWorld is used to implement energy use
+function LifelikeWorld(map, legend) {
+  World.call(this, map, legend);
+}
+LifelikeWorld.prototype = Object.create(World.prototype);
+
+var actionTypes = Object.create(null);
+
+LifelikeWorld.prototype.letAct = function(critter, vector) {
+  var action = critter.act(new View(this, vector));
+  var handled = action &&
+    action.type in actionTypes &&
+    actionTypes[action.type].call(this, critter,
+                                  vector, action);
+  if (!handled) {
+    critter.energy -= 0.2;
+    if (critter.energy <= 0)
+      this.grid.set(vector, null);
+  }
+};
