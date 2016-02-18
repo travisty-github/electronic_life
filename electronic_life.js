f1e23d5695022d7a2efb2665f7a76722a9e0d7c0
Travis <mail@travisbloomfield.com>
2016-02-18 16:16:08 +1100

    Created Grid object to represent items in world.

--- electronic_life.js
+++ electronic_life.js
@@ -20,3 +20,23 @@ function Vector(x, y) {
 Vector.prototype.plus = function(other) {
   return new Vector(this.x + other.x, this.y + other.y);
 };
+
+// Object to represent world grid.
+function Grid(width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+// Validate that the specified vector is within the world grid.
+Grid.prototype.isInside = function(vector) {
+  return vector.x >= 0 && vector.x < this.width &&
+         vector.y >= 0 && vecotr.y < this.height;
+};
+// Return the grid item at the specified vector location.
+Grid.prototype.get = function(vector) {
+  return this.space[vector.x + this.width * vector.y];
+};
+// Set the grid item at the specified vector location.
+Grid.prototype.set = function(vector, value) {
+  this.space[vector.x + this.width * vector.y] = value;
+};
